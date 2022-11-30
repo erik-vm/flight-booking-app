b6ac85ecbae53bff351662fe2a8ec70d2fff46ae
Luis Filipe <luisfelipecorino2@gmail.com>
2022-11-30 16:55:15 +0100

    Added the Filter Component

--- client/src/Components/SecondPage.js
+++ client/src/Components/SecondPage.js
@@ -14,6 +14,7 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Slider from '@mui/material/Slider';
 
 const airlineNames = ["Big Airline", "Modest Airlene", "Roma Airline"];
 
@@ -29,24 +30,18 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function SecondPage(){
 
-    function getAirlines() {
-        return (
-            <FormControl>
-            <FormLabel>Airlines</FormLabel>
+    const GetAirlines = () => {
+        return(
             <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="0"
-                name="radio-buttons-group"
-            >
-                
-                <FormControlLabel value="0" control={<Radio />} label="Female" />
-                <FormControlLabel value="1" control={<Radio />} label="Male" />
-                <FormControlLabel value="2" control={<Radio />} label="Other" />
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        name="radio-buttons-group"
+                        >
+                {airlineNames.map(i =>(
+            <FormControlLabel value = {i} control={<Radio />} label={i} />))}
             </RadioGroup>
-            </FormControl> 
-                        
         );
     }
+
     return(
         
     
@@ -55,22 +50,36 @@ export default function SecondPage(){
             
             <Item>
                 <Grid margin = {3}>
-                <FormLabel >Filters</FormLabel>
+                <FormLabel style= {{color: "blue"}}>Filters</FormLabel>
                 </Grid>
                 <Grid margin = {2}>
             
                     <FormControl>
-                    <FormLabel>Airlines</FormLabel>
+                    <FormLabel style= {{color: "red"}}>Airlines</FormLabel>
+                    <GetAirlines/>
+                        
+                    <FormLabel style= {{color: "red"}}>Scales</FormLabel>
+                    <FormControlLabel control={<Checkbox />} label="No Scales" />
+
+                    <FormLabel style= {{color: "red"}}>Luggage</FormLabel>
                     <RadioGroup
                         aria-labelledby="demo-radio-buttons-group-label"
-                        defaultValue="0"
                         name="radio-buttons-group"
                     >
-                        
-                        <FormControlLabel value="0" control={<Radio />} label="Female" />
-                        <FormControlLabel value="1" control={<Radio />} label="Male" />
-                        <FormControlLabel value="2" control={<Radio />} label="Other" />
+                        <FormControlLabel value="yes" control={<Radio />} label="Allowed" />
+                        <FormControlLabel value="no" control={<Radio />} label="Not Allowed" />
                     </RadioGroup>
+                    <FormLabel style= {{color: "red"}}>Time Range</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="3"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="1" control={<Radio />} label="1 Day" />
+                        <FormControlLabel value="2" control={<Radio />} label="2 Days" />
+                        <FormControlLabel value="3" control={<Radio />} label="3 Days" />
+                    </RadioGroup>
+
                     </FormControl>               
             
                 </Grid>
