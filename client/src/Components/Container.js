d5947225e5ca1f22bcca19454572fb89edba21d1
Luis Filipe <luisfelipecorino2@gmail.com>
2022-11-30 10:44:08 +0100

    Finished trip options and added calendar

--- client/src/Components/Container.js
+++ client/src/Components/Container.js
@@ -1,3 +1,5 @@
+//import * as React from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -8,9 +10,15 @@ import Button from "./Button"
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const originData = ["Origin","Paris","Monaco", "Roma", "Venecia"];
 const destinationData = ["Destination", "Madrid", "Dublin", "Berlin", "Lisbon"];
+
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,7 +28,9 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
+
 export default function Container(){
+    const [value, onChange] = useState(new Date());
     return(
         
     
@@ -29,14 +39,25 @@ export default function Container(){
             <Grid xs={2}>
                 <Button places = {originData} />
             </Grid>
-            <Grid xs={2}>
+            <Grid xs={3}>
                 <Button places = {destinationData} />
             </Grid>
-            <Grid xs={2}>
-                <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label="One-Way" />
-                    <FormControlLabel control={<Checkbox />} label="Round Trip" />
-                </FormGroup>
+            <Grid xs={3}>
+                <FormControl>
+                    <RadioGroup aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="One-Way"
+                                name="radio-buttons-group">
+                    <FormControlLabel value="One-Way" control={<Radio/>} label="One-Way" />
+                    <FormControlLabel value="Round Trip" control={<Radio/>} label="Round Trip" />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid xs={3}>  
+                <Calendar
+                    onChange={onChange}
+                    value={value}
+                />
+
             </Grid>
             </Grid>
         </Box>
